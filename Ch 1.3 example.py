3578a398c648820f4051577a7a5f3a65e8479b1c
Wolfy4704 <braydenwoodard04@gmail.com>
2020-09-11 13:00:10 -0600

    figuring things out

---
+++ Ch 1.3 example.py
@@ -0,0 +1,21 @@
+#Brayden Woodard
+#9/20
+#ch1.3 example 
+
+x = 387/6*55\#backslash breaks up a line of code 
+    +333
+
+print(x)#print function this is a built in function 
+y = 5 #assigning 5 to y 
+print (y)
+
+def test(x);#multiply any number by 5 
+    answer = x + 5
+    print(answer)
+
+
+test(5)
+
+
+#comment for comments sake
+
---
+++ Hello world.py
@@ -0,0 +1,9 @@
+# Brayden Woodard
+# Hello World
+#9/2020
+
+import this
+
+print("Hello World")
+print("this is coding")
+input()

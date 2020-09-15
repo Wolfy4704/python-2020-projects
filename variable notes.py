83a16f7666e7bba8d62d852a101506503032b5ed
Brayden Woodard <brayden.woodard@tooeleschools.org>
2020-09-15 14:12:47 -0600

    adding todays python notes 9/15/2020

---
+++ variable notes.py
@@ -0,0 +1,27 @@
+#Brayden Woodard
+#9/20
+#
+
+
+#1stplayer = "Eric" not a good variable
+#player$1 = "Eric"
+#player1 = "Eric"
+player_1 = "Eric"
+
+SCORE = 99 #all caps name means its a constant
+BDAY = "06/03"
+PIE = 3.14
+
+player = "me"
+myname = "Eric"
+myName
+my_name
+
+number = 5
+nUmber = 5
+nUmBerR = 5
+NuMbEr = 5
+number = 5
+_number = 5
+
+continue #words in orange are key words

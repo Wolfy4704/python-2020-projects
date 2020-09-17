b6bcb25cd109d550558f3b70d2c4c29091e255ae
Brayden Woodard <brayden.woodard@tooeleschools.org>
2020-09-17 14:17:24 -0600

    stuff

---
+++ ch 2.3 High Score.py
@@ -0,0 +1,19 @@
+#Brayden Woodard
+#9/20
+#ch 2.3 High Scores
+
+#SBN means Score Board name these will be the names for the holders of the top 10 high scores 
+
+SBN1 = "WJM" 
+SBN2 = "BBW"
+SBN3 = "MJR"
+SBN4 = "GCW"
+SBN5 = "EMW"
+SBN6 = "
+SBN7 = "
+SBN8 = "
+SBN9 = "
+SBN10 = "
+
+#hight scores for the top 10 players
+HScore1 = "3,333,360"

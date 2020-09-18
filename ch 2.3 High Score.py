f7ca7f5e1d3484295e08cccaf6d135a777cba9c7
Wolfy4704 <braydenwoodard04@gmail.com>
2020-09-18 13:42:43 -0600

    adding the completed high score assignment

--- ch 2.3 High Score.py
+++ ch 2.3 High Score.py
@@ -3,17 +3,65 @@
 #ch 2.3 High Scores
 
 #SBN means Score Board name these will be the names for the holders of the top 10 high scores 
-
 SBN1 = "WJM" 
 SBN2 = "BBW"
 SBN3 = "MJR"
 SBN4 = "GCW"
-SBN5 = "EMW"
-SBN6 = "
-SBN7 = "
-SBN8 = "
-SBN9 = "
-SBN10 = "
+SBN5 = "EjW"
+SBN6 = "mog"
+SBN7 = "wwt"
+SBN8 = "twk"
+SBN9 = "lol"
+SBN10 = "wap"
 
 #hight scores for the top 10 players
-HScore1 = "3,333,360"
+h_score1 = "3,333,360"
+h_score2 = "2,936,392"
+h_score3 = "2,355,500"
+h_score4 = "2,179,554"
+h_score5 = "2,160,448"
+h_score6 = "1,958,420"
+h_score7 = "1,790,222"
+h_score8 = "1,649,167"
+h_score9 = "1,561,985"
+h_score10 = "1,090,257"
+
+midline = "--------"
+midline2 = "----------------------------"
+bottomline = "________________"
+title = "--------HighScore-------"
+
+#printing the score bored
+print("|" + title + "|")
+
+print("|" + SBN1 + midline + h_score1 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN2 + midline + h_score2 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN3 + midline + "-" + h_score3 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN4 + midline + h_score4 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN5 + midline + "-" + h_score5 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN6 + midline + "-" + h_score6 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN7 + midline + "-" +h_score7 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN8 + midline + "--" + h_score8 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN9 + midline + "----" + h_score9 + "|")
+print("|" + midline2 + "|")
+
+print("|" + SBN10 + midline + "-" + h_score10 + "|")
+print("|" + midline2 + "|")
+
+

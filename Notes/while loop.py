c647613f711b7531399cffe77fbb4a504bdfffa8
Brayden Woodard <brayden.woodard@tooeleschools.org>
2020-10-13 14:14:55 -0600

    adding hangman game

---
+++ Notes/while loop.py
@@ -0,0 +1,16 @@
+##x = 0
+##while x <1000000000000000000000:
+##    print(x,end = " ")
+##    x+=1
+##WORDS = ("OVERUSED","CLAM","GUAM","COOKIE","PYTHON")
+##for i in WORDS:
+##    print(i)
+##
+##    for x in i:
+##        print(x)
+##        input()
+
+
+
+for i in range(10000000000000000000000):
+    print(i)
--- hangman.py
+++ hangman.py
@@ -82,45 +82,55 @@ word = random.choice(WORDS)
 #assignment
 #10 python related word with one refering to the snake
 #deffine the terms in comments
+#after the word is revealed show definition to user 
 wrong = 0
 used = []
 so_far = "*"*len(word)
 
 print("Welcome to Hangman. Good luck!")
-print()
-print(HANGMAN[wrong])
-print(so_far)
+
+while wrong < MAX_WRONG and so_far != word:
+    print(HANGMAN[wrong])
+    print("\nYouve used the following letters:\n",used)
+    print("\nSo far the word is:\n",so_far)
+    guess = input("\n\nEnter your guess: ")
+    guess = guess.upper()
+    print(guess)
 
 
+    while guess in used:
+        print("youve already guessed that letter",guess)
+        guess = input("Enter your guess: ")
+        guess = guess.upper()
+
+    used.append(guess)
+
+    if guess in word:
+        print("\nYes!", guess, "is in the word!")
+
+        #create new so_far to include guess in the correct location
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new+=guess
+            else:
+                new+=so_far[i]
+        so_far = new
+
+    else:
+        print("\nSorry", guess, "isnt in the word.")
+        wrong+=1
 
 
+if wrong == MAX_WRONG:
+    print(HANGMAN[wrong])
+    print("\nYouve been hanged!")
+else:
+    print("\nYou Guessed it!")
 
+print("\nThe word was", word)
 
-
-
-##option1
-##index = random.randint(0,len(WORDS)-1)
-##word = WORDS[index]
-##print(word)
-
-
-##x = 0
-##while x < len(HANGMAN):
-##    print(HANGMAN[x])
-##    input()
-##    x+=1
-
-
-
-
-
-
-
-
-
-
-
-
+input("\n\nPress the enter key to exit")
 
 
 

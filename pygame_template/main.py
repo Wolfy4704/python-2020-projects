c1bb6bc329bf4e5059b1169889fa8be5ee7296cb
Brayden Woodard <brayden.woodard@tooeleschools.org>
2021-02-23 14:15:35 -0700

    das

---
+++ pygame_template/main.py
@@ -0,0 +1,31 @@
+import pygame as pg
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPs = 30
+title = "Template"
+
+#color
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+
+pg.init()
+pg.micer.init()
+
+screen=pg.display.set_mode((WIDTH,HEIGHT))
+pg.display.set_caption(title)
+clock = pg.time.Clock()
+
+
+
+#game loop
+running = True
+while running:
+    #process input
+
+    #make updates
+
+    #render (Draw)
+
